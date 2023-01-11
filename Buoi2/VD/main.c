2e7c6e418fb06eb7a48766a7a3284fa73f6f0b27
DaoDucPhu2002 <daophu2002dkm@gmail.com>
2023-01-12 00:56:11 +0700

    Update Proteus and coding VD

--- Buoi2/VD/main.c
+++ Buoi2/VD/main.c
@@ -1,30 +1,32 @@
-#include <P18f3420.h>
-
+#include <P18f4520.h>
+#include <delays.h>
+//cau hinh PIC
 #pragma config  OSC = HS //Che do giao dong HS
 #pragma	config	MCLRE = ON //su dung chan RE la chan
 #pragma config	PBADEN = OFF // cau hinh port b la so
-
-
-//BTN = R3 
-//LED = R7
+//Define IO
 #define BTN PORTBbits.RB3 // dinh nghia BTN = RB3
-#define LED PORTBbits.R7 // Dinh nghia LED = R7
+#define LED PORTBbits.RB7 // Dinh nghia LED = R7
+
+void delay_second(int time)
+{
+	int i;
+	for (i=0;i<time;i++)
+	{
+		Delay10KTCYx(200);//tre 1s (thach anh = 8MHz) = 200*1/8MHz )	
+	}
+}
 
 void main()
 {
-int giatri_BTN = BTN ;
-LED = 0;
-
 while(1)
 	{
-		if giattri_BTN == 1
-		{
-			LED = 1	;
-		}
-		else
-			LED = 0;
+	LED = 1;
+	delay_second(1);
+	
 	}
 
-return 0;	
 
 }
+
+
