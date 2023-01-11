11215ef33e2271b8215ea69286dc2ea79738a552
DaoDucPhu2002 <daophu2002dkm@gmail.com>
2023-01-11 23:50:02 +0700

    Update Buoi2 Vidu

--- Buoi2/VD/main.c
+++ Buoi2/VD/main.c
@@ -1,1 +1,30 @@
 #include <P18f3420.h>
+
+#pragma config  OSC = HS //Che do giao dong HS
+#pragma	config	MCLRE = ON //su dung chan RE la chan
+#pragma config	PBADEN = OFF // cau hinh port b la so
+
+
+//BTN = R3 
+//LED = R7
+#define BTN PORTBbits.RB3 // dinh nghia BTN = RB3
+#define LED PORTBbits.R7 // Dinh nghia LED = R7
+
+void main()
+{
+int giatri_BTN = BTN ;
+LED = 0;
+
+while(1)
+	{
+		if giattri_BTN == 1
+		{
+			LED = 1	;
+		}
+		else
+			LED = 0;
+	}
+
+return 0;	
+
+}
