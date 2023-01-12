438aeeea63092fd0c90b919eee3c779c1377628b
DaoDucPhu2002 <daophu2002dkm@gmail.com>
2023-01-12 18:48:40 +0700

    Adding IHW2.1

--- Buoi2/IHW2.1/main.c
+++ Buoi2/IHW2.1/main.c
@@ -2,11 +2,32 @@
 #include <delays.h>
 
 #pragma config OSC = HS // thiet lap che do xung
-#pragma config MCRLE = ON //thiet lap chan RE
+#pragma config MCLRE = ON //thiet lap chan RE
 
+#define KEY PORTDbits.RD7
+
+int x = 0x01;
 void main(void)
 {
-
-
-
+	TRISC = 0x00;
+	TRISB = 0x80;
+	ADCON1 = 0x0f;
+	while(1)
+	{
+		if (KEY == 1)
+		{
+			PORTC = x;
+			x=x<<1;	
+			Delay10KTCYx(100);//1s 
+			if (x > 0x08)
+			{
+				x = 0x01;
+			}
+		}
+		else
+		{
+			PORTC = 0x00;
+		}
+		
+	}
 }
